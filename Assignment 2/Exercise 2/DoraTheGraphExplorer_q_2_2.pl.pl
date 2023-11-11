b61f1ddda70248ca2c2d28511ffbba3f1695c799
Nobel <nobelchua@yahoo.com.sg>
2023-11-12 02:22:20 +0800

    prolog code

--- Assignment 2/Exercise 2/DoraTheGraphExplorer_q_2_2.pl.pl
+++ Assignment 2/Exercise 2/DoraTheGraphExplorer_q_2_2.pl.pl
@@ -20,7 +20,7 @@ precedes(X,Y):- male(X),male(Y),is_older(X,Y).
 precedes(X,Y):- male(X),female(Y),Y\=elizabeth.
 precedes(X,Y):- female(X),female(Y),is_older(X,Y).
 
-insert(A,[B|C],[B|D]):- not(precedes(A,B)),!,insert(A,C,D).
+insert(A,[B|C],[B|D]):- not(is_older(A,B)),!,insert(A,C,D).
 insert(A,C,[A|C]).
 succession_sort([A|B],SortList):- succession_sort(B,Tail),insert(A,Tail,SortList).
 succession_sort([],[]).
