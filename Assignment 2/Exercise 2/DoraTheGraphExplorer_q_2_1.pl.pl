fa36ff7e4d2e83a3d31836cdeabf72d8ed303b7f
Nobel <nobelchua@yahoo.com.sg>
2023-11-11 16:58:40 +0800

    2.1 prolog file

--- Assignment 2/Exercise 2/DoraTheGraphExplorer_q_2_1.pl.pl
+++ Assignment 2/Exercise 2/DoraTheGraphExplorer_q_2_1.pl.pl
@@ -1,17 +1,16 @@
-% Define the relations
+
 male(prince_charles).
 male(prince_andrew).
 male(prince_edward).
 female(princess_ann).
 female(queen_elizabeth).
 
-% Define the parent-child relationships
+
 parent(queen_elizabeth, prince_charles).
 parent(queen_elizabeth, princess_ann).
 parent(queen_elizabeth, prince_andrew).
 parent(queen_elizabeth, prince_edward).
 
-% Define the old Royal succession rule
 successor(X, Y) :-
     male(X),                % X is male
     parent(Z, X),           % Z is a parent of X
@@ -19,7 +18,6 @@ successor(X, Y) :-
     X \= Y,                 % X is not the same as Y
     Y \= princess_ann.      % Y is not princess_ann
 
-% Define the first in line of succession
 successor_line(X) :-
     parent(queen_elizabeth, X),
     X \= princess_ann.
